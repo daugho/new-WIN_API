3968d47cdcebdb30867b0f397b7aa0149db4e11d
cooljyum <yhj116@naver.com>
2025-06-05 17:51:10 +0900

    1

--- newWINAPI/Resource.h
+++ newWINAPI/Resource.h
@@ -1,17 +1,17 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// 다음에서 사용 newWINAPI.rc
+// 다음에서 사용 WinAPI_2504.rc
 
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_NEWWINAPI_DIALOG	102
+#define IDD_WINAPI2504_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_NEWWINAPI			107
+#define IDI_WINAPI2504			107
 #define IDI_SMALL				108
-#define IDC_NEWWINAPI			109
+#define IDC_WINAPI2504			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
--- newWINAPI/framework.h
+++ newWINAPI/framework.h
@@ -1,15 +1,36 @@
-﻿// header.h: 표준 시스템 포함 파일
-// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
-//
-
-#pragma once
+﻿#pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
-// Windows 헤더 파일
+#define WIN32_LEAN_AND_MEAN
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 800
+
+#define DELTA Timer::Get()->GetElapsedTime()
+
 #include <windows.h>
-// C 런타임 헤더 파일입니다.
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#include "Framework/Utilities/Singleton.h"
+#include "Framework/Utilities/Timer.h"
+#include "Framework/Utilities/Input.h"
+
+#include "Framework/Math/Vector2.h"
+
+#include "Objects/PaintTool.h"
+#include "Objects/Circle.h"
+#include "Objects/Player.h"
+#include "Objects/Bullet.h"
+#include "Objects/BulletManager.h"
+#include "Objects/Enemy.h"
+#include "Objects/EnemyManager.h"
+
+#include "Scenes/Scene.h"
+#include "Framework/GameManager.h"
+
+extern HWND hWnd;
+extern POINT mousePos;
